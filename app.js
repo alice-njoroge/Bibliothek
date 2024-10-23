2b5a645ceba45e228850b9bd8f88bb355171ac4c
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-10-23 10:28:58 +0300

    creating DOM nodes for better performance

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
+//create a single row
+function createTableRow(book) {
+    //  <tr class="heading-data">
+    //     <th>ISBN</th>
+    //     <th>Title</th>
+    //     <th>Details</th>
+    //  </tr>
 
-function addBooksToUI(books){
-    let tableRowHTMLString = '';
-    for(let book of books){
-        let rowString = `
-        <tr>
-            <td>${book.isbn}</td>
-            <td>${book.title}</td>
-            <td>Deets</td>
-        </tr>`
-        tableRowHTMLString += rowString
+    const row = document.createElement('tr');
+
+    const isbnCell = document.createElement('td');
+    isbnCell.textContent = book.isbn;
+    const titleCell = document.createElement('td');
+    titleCell.textContent = book.title;
+    const detailsCell = document.createElement('td');
+
+    //<a href="link">Text</a>
+    const aTag = document.createElement('a');
+    aTag.href = "about.html"
+    aTag.textContent = book.id;
+    detailsCell.appendChild(aTag);
+
+    row.appendChild(isbnCell);
+    row.appendChild(titleCell);
+    row.appendChild(detailsCell);
+
+    return row;
+}
+
+//render multiple rows to the UI
+function addBooksToUI(books) {
+    const tableRow = document.querySelector('.books-rows');
+    for (let book of books) {
+        let bookRow = createTableRow(book);
+        tableRow.appendChild(bookRow);
     }
-    document.querySelector('.books-rows').innerHTML = tableRowHTMLString;
 
 }
+
 //fetch the books from the api
 async function fetchBooks() {
     try {
@@ -28,6 +52,7 @@ async function fetchBooks() {
         console.log("oops there was an issue with the fetch operation", error)
     }
 }
+
 fetchBooks();
 
 
